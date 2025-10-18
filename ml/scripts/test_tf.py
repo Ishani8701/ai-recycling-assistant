60a3790652a896cf07b5bc295f0df03f0efc77fa
ghostxic <mcl14@illinois.edu>
2025-10-17 21:42:07 -0500

    Added code to ml folder to download simpler kaggle dataset

--- ml/scripts/test_tf.py
+++ ml/scripts/test_tf.py
@@ -1,2 +1,10 @@
 import tensorflow as tf
+import kagglehub
+import pandas as pd
+import os
+os.environ["KAGGLEHUB_CACHE"] = "./kaggle_downloads"
+
+dataset = "ashwinshrivastav/most-common-recyclable-and-nonrecyclable-objects"
+kagglehub.dataset_download(dataset)
+
 print("TensorFlow version:", tf.__version__)
