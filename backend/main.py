c2a2fc0920a27a52a91974f3c87e48c67ea191b8
Noah Kang <97856256+noah-the-kang@users.noreply.github.com>
2025-10-25 14:24:14 -0500

    added prediction endpoints

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, UploadFile, File
+from typing import Dict
 
-app = FastAPI()
-
+app = FastAPI(title="Recyclable Object Detector API")
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hello World. API is running."}
+
+
+@app.post("/predict")
+async def predict_json(data: Dict):
+    # log the received data
+    print(f"Received JSON data for mock prediction: {data}")
+
+    return {"result": "recyclable"}
+
+
+@app.post("/predict/image")
+async def predict_image(file: UploadFile = File(...)):
+    """
+    Accepts an uploaded image file (via multipart/form-data) 
+    and returns a mock prediction.
+    """
+    image_bytes = await file.read()
+    
+    print(f"Received file: {file.filename}")
+    print(f"File size: {len(image_bytes)} bytes")
+
+    return {"result": "recyclable"}
